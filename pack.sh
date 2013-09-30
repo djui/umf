232c04a2aeb0a31bae1ff119e4a82293c3dbac8f
Uwe Dauernheim <uwe@dauernheim.net>
2013-09-30 22:38:49 +0200

    Move output to bin
    Change-Id: Iade589ac22cc1b25d423c9aa03d06e1284db138f

--- pack.sh
+++ pack.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 SRC=game.py
-BIN=play.sh
+BIN=bin/play.sh
 
+mkdir -p bin
 xz -9e --keep --force $SRC
 cat header.sh $SRC.xz > $BIN
 chmod 0777 $BIN
+
+# Just for comparison
+python -m compileall . > /dev/null
