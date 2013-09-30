a38e2658171864b6f56c9371b4f9005c3dbd491a
Per Lindstrand <fpj@spotify.com>
2013-09-30 23:53:41 +0200

    Draw player and fix fog of war

--- game.py
+++ game.py
@@ -9,12 +9,10 @@ SCR.keypad(1)
 SCRH, SCRW = SCR.getmaxyx()
 
 TILEW, TILEH = 5, 3
-MSIZE = 100
-MAZE = [ri(0, 1) for x in range(MSIZE * MSIZE)]
-SHADE = [' ', curses.ACS_BOARD, curses.ACS_CKBOARD]
-
-VX, VY = 0, 0
-PX, PY = 0, 0
+MAZEW, MAZEH = 100, 100
+MAZE = [ri(0, 1) for x in range(MAZEW * MAZEH)]
+SHADE = [' ', '.', ',', '+', curses.ACS_PLMINUS, '#', curses.ACS_CKBOARD, chr(178)]
+PTILE = 255
 
 
 def genmaze(s, w, h):
@@ -25,30 +23,45 @@ def genmaze(s, w, h):
     return m
 
 
-def shade(b):
-    return SHADE[b * len(SHADE) / 256]
-
-
-def drawtile(t, tw, th, tx, ty, px, py, sx, sy):
-    if t == 0:
-        b = 0
+def shade(ch, b):
+    if ch in SHADE:
+        i = int(b * float(SHADE.index(ch)))
+        return SHADE[max(0, min(i, len(SHADE) - 1))]
     else:
-        b = 255
+        return ch
 
-    for x in range(max(0, sx), min(SCRW, sx + tw)):
-        for y in range(max(0, sy), min(SCRH, sy + th)):
+
+def drawtile(t, tx, ty, px, py, sx, sy):
+    if t == 0:
+        ch = ' '
+    elif t == 1:
+        ch = curses.ACS_CKBOARD
+    elif t == 255:
+        ch = curses.ACS_DIAMOND
+
+    d = sqrt((px - tx) * (px - tx) + (py - ty) * (py - ty))
+    df = 5.0 / max(1.0, d)
+
+    for x in range(max(0, sx), min(SCRW, sx + TILEW)):
+        for y in range(max(0, sy), min(SCRH, sy + TILEH)):
             try:
-                d = 0.5 * sqrt((px - tx) * (px - tx) + (py - ty) * (py - ty))
-                bb = min(255, max(0, b / int(max(1, d))))
-                SCR.addch(1 + y, 1 + x, shade(bb))
+                SCR.addch(1 + y, 1 + x, shade(ch, df))
             except curses.error:
                 pass
 
 
-def drawmaze(m, mw, mh, px, py, vx, vy, vw, vh):
-    for x in range(max(0, vx), min(mw, vx + vw)):
-        for y in range(max(0, vy), min(mh, vy + vh)):
-            drawtile(m[x + y * mw], TILEW, TILEH, x, y, px, py, (x - vx) * TILEW, (y - vy) * TILEH)
+def drawmaze(px, py, vx, vy, vw, vh):
+    for x in range(max(0, vx), min(MAZEW, vx + vw)):
+        for y in range(max(0, vy), min(MAZEH, vy + vh)):
+            drawtile(MAZE[x + y * MAZEW], x, y, px, py, (x - vx) * TILEW, (y - vy) * TILEH)
+
+
+def canmove(x, y):
+    return 0 <= x < MAZEW and 0 <= y < MAZEH and MAZE[x + y * MAZEW] == 0
+
+
+def idx(x, y):
+    return x + y * MAZEW
 
 
 def main():
@@ -56,23 +69,38 @@ def main():
 
     playing = True
 
-    vx, vy = 0, 0
+    old = 0
+    px, py = 10, 10
 
     while playing:
         SCRH, SCRW = SCR.getmaxyx()
 
         SCR.erase()
 
-        drawmaze(MAZE, MSIZE, MSIZE, 10, 10, vx, vy, SCRW - 1, SCRH - 1)
+        vw, vh = SCRW // TILEW, SCRH // TILEH
+        vx, vy = px - vw // 2, py - vh // 2
+        drawmaze(px, py, vx, vy, vw, vh)
+
+        MAZE[idx(px, py)] = old
 
         ch = SCR.getch()
-        if ch == curses.KEY_UP: vy -= 1
-        elif ch == curses.KEY_DOWN: vy += 1
-        elif ch == curses.KEY_LEFT: vx -= 1
-        elif ch == curses.KEY_RIGHT: vx += 1
-        elif ch == 27: playing = False
+        if ch == curses.KEY_UP and canmove(px, py - 1):
+            py -= 1
+        elif ch == curses.KEY_DOWN and canmove(px, py + 1):
+            py += 1
+        elif ch == curses.KEY_LEFT and canmove(px - 1, py):
+            px -= 1
+        elif ch == curses.KEY_RIGHT and canmove(px + 1, py):
+            px += 1
+        elif ch == 27:
+            playing = False
 
-        SCR.addstr(2, 2, str(int(ch)))
+        MAZE[idx(px, py)] = PTILE
+
+        # ai
+
+        SCR.addstr(1, 2, str(int(ch)))
+        SCR.addstr(2, 2, str(px) + ',' + str(py))
 
         SCR.move(SCRH - 1, SCRW - 1)
         SCR.refresh()
