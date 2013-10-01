dc3132463304c04a5bc22f0187b7fa53f1e1e3aa
Uwe Dauernheim <uwe@dauernheim.net>
2013-10-01 05:38:02 +0200

    WIP
    Change-Id: I6b1fdd65edeef8c6878c96bcd8328a98ddaee44d

--- pack.sh
+++ pack.sh
@@ -6,7 +6,7 @@ mkdir -p bin
 
 # Pack
 ./mnfy game.py > game.min.py
-tar -cf - game.min.py music.xm die.xm | xz -9e -c > pack.txz
+tar -cf - game.min.py music.xm | xz -9e -c > pack.txz
 
 cat header.sh pack.txz > $BIN
 chmod 0777 $BIN
